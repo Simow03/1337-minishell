d478ea799cf8cdd37670c437ad34ad99fd6f6bfc
mstaali <imthetigerz@gmail.com>
2024-05-03 19:23:03 +0100

    fix_echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,26 +6,40 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 20:22:28 by mstaali           #+#    #+#             */
-/*   Updated: 2024/05/01 22:59:37 by mstaali          ###   ########.fr       */
+/*   Updated: 2024/05/03 19:22:33 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	is_flag(char *cmd)
+{
+	int	i;
+
+	if (cmd[0] != '-')
+		return (0);
+	i = 1;
+	while (cmd[i] && cmd[i] == 'n')
+		i++;
+	if (!cmd[i])
+		return (1);
+	return (0);
+}
 
 void	echo(char **cmd)
 {
 	int	i;
 
 	i = 1;
-	if (ft_strncmp(cmd[1], "-n", 2) == 0 && ft_strlen(cmd[1]) == 2)
-		i = 2;
-    while (cmd[i])
-    {
-        ft_putstr_fd(cmd[i], 1);
-        if (cmd[i + 1])
-            ft_putstr_fd(" ", 1);
-        i++;
-    }
-	if (ft_strncmp(cmd[1], "-n", 2) != 0 && ft_strlen(cmd[1]) == 2)
-    	ft_putstr_fd("\n", 1);
+	while (is_flag(cmd[i]))
+		i++;
+	while (cmd[i])
+	{
+		ft_putstr_fd(cmd[i], 1);
+		if (cmd[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!is_flag(cmd[1]))
+		ft_putstr_fd("\n", 1);
 }
