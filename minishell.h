e2f9bf86ac5e639ef021057d72cf67d638b31929
mstaali <imthetigerz@gmail.com>
2024-05-05 20:55:14 +0100

    .h_

--- minishell.h
+++ minishell.h
@@ -4,36 +4,51 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 
-typedef    struct s_here_doc
+typedef struct s_env
 {
-	//pipe's file descriptor 
-	int        fd;
-	char    *delimeter;
-	//checks if the delimeter is quoted, if so the input should not be expanded.
-	int        deli_quoted;
-}    t_here_doc;
+	char			*name;
+	char			*value;
+	struct s_env	*next;
+} t_env;
+
+//red_mod:    redirection type:
+//0:    <
+//1:    >
+//2:    >>
+
+typedef    struct s_files
+{
+        char            *file;
+        int                red_mode;
+        struct s_files    *next;
+}    t_files;
 
 typedef    struct s_cmd
 {
-	//command where it will be protoyped like this: ["command", "argument1", ... "argumentn", NULL].
-	char            **cmd; 
-	//input string, if NULL read from standard input, if "here_doc" read from the here doc.
-	char            input;
-	//info about the here_doc
-	t_here_doc        *here_doc;
-	//output string, if NULL write in standard output
-	char            *output;
-	//redirection type, if 0 it means ">", if 1 it means ">>".
-	int                red_mode;
-	struct s_cmd    *next_cmd;
+    //command where it will be protoyped like this: ["command", "argument1", ... "argumentn", NULL].
+    char            **cmd;
+    //input string, if NULL read from standard input, 
+    t_files            *input;
+    //output string, if NULL write in standard output
+    t_files            *output;
+    struct s_cmd    *next;
 }    t_cmd;
-
 
 //------  LIBFT  -------//
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 int		ft_strncmp(const char *str1, const char *str2, size_t size);
 size_t	ft_strlen(const char *str);
+char	**ft_split(char const *s, char c);
+t_env	*ft_env_lstnew(char *name, char *value);
+void	ft_envadd_back(t_env **var, t_env *new);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	**ft_envsplit(char const *s);
+char	*ft_strdup(const char *str);
+t_env	*ft_lstlast(t_env *env);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
 
 #endif
