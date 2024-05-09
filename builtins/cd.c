0590e1de5aa066a38150a58b1f5f8ed4f9e5b3bc
mstaali <imthetigerz@gmail.com>
2024-05-09 17:30:15 +0100

    cd_init

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,11 +6,56 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 23:37:20 by mstaali           #+#    #+#             */
-/*   Updated: 2024/05/05 21:04:24 by mstaali          ###   ########.fr       */
+/*   Updated: 2024/05/08 21:33:58 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void cd(char **cmd)
+#include "../minishell.h"
+
+void	get_home_dir(t_env **env, char *old_pwd)
 {
-	
+	char	*home;
+
+	while ((*env))
+	{
+		if (ft_strcmp((*env)->name, "HOME") == 0)
+            home = (*env)->value;
+        (*env) = (*env)->next;
+	}
+	while ((*env))
+	{
+		if (ft_strcmp((*env)->name, "OLDPWD") == 0)
+            (*env)->value = old_pwd;
+        if (ft_strcmp((*env)->name, "PWD") == 0)
+            (*env)->value = home;
+        (*env) = (*env)->next;
+	}
+}
+
+int	cd(char **cmd, t_env **env, char *prompt)
+{
+	char	*curr_pwd;
+	char	*old_pwd;
+
+	old_pwd = getcwd(NULL, 0);
+	if (cmd[1])
+	{
+        if (chdir(cmd[1]) == -1)
+		{
+            printf("%s: cd: %s: No such file or directory\n", prompt, cmd[1]);
+			return (1);
+		}
+		curr_pwd = cmd[1];
+		while ((*env))
+		{
+			if (ft_strcmp((*env)->name, "OLDPWD") == 0)
+                (*env)->value = old_pwd;
+            if (ft_strcmp((*env)->name, "PWD") == 0)
+                (*env)->value = curr_pwd;
+            (*env) = (*env)->next;
+		}
+    }
+	else
+		get_home_dir(env, old_pwd);
+	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -13,29 +13,6 @@ typedef struct s_env
 	struct s_env	*next;
 } t_env;
 
-//red_mod:    redirection type:
-//0:    <
-//1:    >
-//2:    >>
-
-typedef    struct s_files
-{
-        char            *file;
-        int                red_mode;
-        struct s_files    *next;
-}    t_files;
-
-typedef    struct s_cmd
-{
-    //command where it will be protoyped like this: ["command", "argument1", ... "argumentn", NULL].
-    char            **cmd;
-    //input string, if NULL read from standard input, 
-    t_files            *input;
-    //output string, if NULL write in standard output
-    t_files            *output;
-    struct s_cmd    *next;
-}    t_cmd;
-
 //------  LIBFT  -------//
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
