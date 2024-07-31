fe1641159b87c1303d75d03e3f4734a5494f9146
mstaali <imthetigerz@gmail.com>
2024-07-31 21:37:22 +0100

    fix_signal

--- execution/signals.c
+++ execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 13:51:38 by mstaali           #+#    #+#             */
-/*   Updated: 2024/07/31 20:10:17 by mstaali          ###   ########.fr       */
+/*   Updated: 2024/07/31 21:31:40 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,16 @@ void	main_signo(int signo)
 	if (signo == SIGINT)
 	{
 		g_sigint_received = 1;
-		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		rl_replace_line("", 0);
 		rl_on_new_line();
+		rl_replace_line("", 0);
+		ft_putstr_fd("\n", 1);
+		rl_redisplay();
 		global_return_int(1, 1);
 	}
 }
 
 void	signal_listener(void)
 {
-	rl_set_signals();
 	signal(SIGINT, main_signo);
 	signal(SIGQUIT, SIG_IGN);
 }
